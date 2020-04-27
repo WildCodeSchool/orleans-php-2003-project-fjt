21864ba06be0cf4b83c24d712ebfcb4d051e73e8
heldeenn <amandine.ln7@gmail.com>
2020-04-27 18:18:31 +0200

    ajout insert animation dans model AnimationManager

--- src/Model/AnimationManager.php
+++ src/Model/AnimationManager.php
@@ -26,4 +26,22 @@ class AnimationManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+
+    /**
+     * @param array $animation
+     * @return int
+     */
+    public function insert(array $animation): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `description`, `image`)
+        VALUES (:name, :description, :image)");
+        $statement->bindValue('name', $animation['name'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $animation['description'], \PDO::PARAM_STR);
+        $statement->bindValue('image', $animation['image'], \PDO::PARAM_STR);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
 }
