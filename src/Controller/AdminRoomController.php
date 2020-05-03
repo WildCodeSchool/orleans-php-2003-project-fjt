ed94015fd652b12bca7618cf6a082ca0d3182acb
Julien Gault <jugault45@gmail.com>
2020-05-03 19:04:53 +0200

    création de la partie ajout d'adresse

--- src/Controller/AdminRoomController.php
+++ src/Controller/AdminRoomController.php
@@ -3,13 +3,13 @@
 
 namespace App\Controller;
 
-use App\Model\AdminRoomManager;
+use App\Model\RoomManager;
 
 class AdminRoomController extends AbstractController
 {
     public function index()
     {
-        $adminRoomManager = new AdminRoomManager();
+        $adminRoomManager = new RoomManager();
         $rooms = $adminRoomManager->selectRoomByAddress();
         $roomByAddresses = [];
         foreach ($rooms as $room) {
@@ -17,5 +17,35 @@ class AdminRoomController extends AbstractController
             $roomByAddresses[$roomName][] = $room;
         }
         return $this->twig->render('AdminRoom/index.html.twig', ['roomByAddresses' => $roomByAddresses]);
+    }
+    public function addAddress()
+    {
+        $adminRoomManager = new RoomManager();
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST)) {
+            $data = array_map('trim', $_POST);
+            $errors = $this->controlAddress($data);
+            if (empty($errors)) {
+                $adminRoomManager->insertAddress($data);
+                header('Location:/AdminRoom/index');
+            }
+        }
+        return $this->twig->render('AdminRoom/addAddress.html.twig', ['post'=> $_POST ?? [], 'errors'=> $errors ?? []]);
+    }
+    private function controlAddress($data)
+    {
+        $errors = [];
+        if (empty($data['name'])) {
+            $errors['name'] = 'Le nom de l\'adresse ne doit pas être vide';
+        } elseif (strlen($data['name']) > 255) {
+            $errors['name'] = 'Le nom du logement est trop long';
+        }
+        if (empty($data['address'])) {
+            $errors['address'] = 'L\'adresse du logement ne doit pas être vide';
+        } elseif (strlen($data['address']) > 255) {
+            $errors['address'] = 'L\'adresse du logement est trop longue';
+        }
+
+        return $errors ?? [];
     }
 }
--- src/Model/RoomManager.php
+++ src/Model/RoomManager.php
@@ -30,4 +30,19 @@ class RoomManager extends AbstractManager
         return $this->pdo->query('SELECT * FROM room JOIN address ON room.address_id=address.id ORDER BY 
         address.id ASC, room.area ASC')->fetchAll();
     }
+    public function selectRoomByAddress(): array
+    {
+        return $this->pdo->query('SELECT * FROM room r JOIN address ON r.address_id = address.id ORDER BY 
+        address.id ASC, r.area ASC')->fetchAll();
+    }
+    public function insertAddress(array $data): void
+    {
+        $statement = $this->pdo->prepare('INSERT INTO address (`name`,`address`,`description`) VALUES
+         (:name,:address,:description)');
+
+        $statement->bindValue(':name', $data['name'], \PDO::PARAM_STR);
+        $statement->bindValue(':address', $data['address'], \PDO::PARAM_STR);
+        $statement->bindValue(':description', $data['description'], \PDO::PARAM_STR);
+        $statement->execute();
+    }
 }
