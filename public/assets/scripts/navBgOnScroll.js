c6c5b05bb73801b07f5fa2767ff3372422836a2a
AnthonyBerduck <byvune@gmail.com>
2020-05-13 15:19:57 +0200

    fixBgResponsive

--- public/assets/scripts/navBgOnScroll.js
+++ public/assets/scripts/navBgOnScroll.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop()>100){
-            $("#navbar").css("background-color","rgb(255, 0, 83)");
-        }else{
-            $("#navbar").css("background-color","rgba(0,0,0,0.0)");
+        if(window.width()<991) {
+            if ($(this).scrollTop() > 100) {
+                $("#navbar").css("background-color", "rgb(255, 0, 83)");
+            } else {
+                $("#navbar").css("background-color", "rgba(0,0,0,0.0)");
+            }
         }
     })
 })
