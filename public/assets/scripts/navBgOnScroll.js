acab9e733b520bcc8b2a7539cf7dfb90b0b9a2f9
AnthonyBerduck <byvune@gmail.com>
2020-05-13 15:32:44 +0200

    fixed

--- public/assets/scripts/navBgOnScroll.js
+++ public/assets/scripts/navBgOnScroll.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if(window.width()<991) {
+        if(window.innerWidth>991) {
             if ($(this).scrollTop() > 100) {
                 $("#navbar").css("background-color", "rgb(255, 0, 83)");
             } else {
                 $("#navbar").css("background-color", "rgba(0,0,0,0.0)");
             }
+        }else{
+            $("#navbar").css("background-color", "rgb(255, 0, 83)");
         }
     })
 })
